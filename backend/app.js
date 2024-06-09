f37b876aee46a33de814a36c03c365086f2c5493
mandagalladinesh <mandagalladinesh@gmail.com>
2024-06-09 11:23:58 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
 const MONGO_URL =
-  "mongodb+srv://hotel_booking:starnivas123@cluster0.9qpuxmc.mongodb.net/hotel_booking?retryWrites=true&w=majority";
+  "mongodb+srv://mdinesh3026:04ZkCRr7o97FZioK@cluster0.kmvan50.mongodb.net/hotel_booking?retryWrites=true&w=majority";
 const PORT = 8000;
 
 const app = express();
