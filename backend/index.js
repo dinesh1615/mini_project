4c53f5846e1cb7e722069f841778344a4d54f297
Nithin Ambati <nithinambati@Nithins-MacBook-Air.local>
2024-06-09 11:54:53 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
 const MONGO_URL =
-  "mongodb+srv://mdinesh3026:04ZkCRr7o97FZioK@cluster0.kmvan50.mongodb.net/hotel_booking?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://mdinesh3026:04ZkCRr7o97FZioK@cluster0.kmvan50.mongodb.net/hotel_booking?retryWrites=true&w=majority";
 const PORT = 8000;
 
 const app = express();
